5eb38650bbf56051b67364cea667bfeb22f8d8a9
Margarita <bano4kaa@mail.ru>
2023-02-09 15:39:59 +0300

    hw5 done

---
+++ Case3/HW.js
@@ -0,0 +1,25 @@
+// Задание 3
+// Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+
+products.forEach(element => {
+    element.price *= 0.15;
+}
+);
+
+console.log(products);
